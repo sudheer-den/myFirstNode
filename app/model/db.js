2c9e3588306280e2904c06ef2711620ec9439450
sudheer vanapalli <sudheer.contactme@gmail.com>
2020-08-29 12:15:24 +0530

    db change

--- app/model/db.js
+++ app/model/db.js
@@ -3,10 +3,14 @@ const dbConfig = require("../config/db.config");
 
 //create connection to db
 const connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b1410d5b9f62d4',
+    password: '3de289a3',
+    database: 'heroku_12e8d63e3685c45'
+    // host: dbConfig.HOST,
+    // user: dbConfig.USER,
+    // password: dbConfig.PASSWORD,
+    // database: dbConfig.DB
 });
 
 connection.connect(error => {
